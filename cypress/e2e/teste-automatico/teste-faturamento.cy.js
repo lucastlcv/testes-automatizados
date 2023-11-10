f5ccc393edaf9179fda859db83062cc9e4b38ebd
Lucas <lucas.carvalho@milvus.com.br>
2023-11-10 17:40:31 -0300

    Andamento da automação de faturamento

--- cypress/e2e/teste-automatico/teste-faturamento.cy.js
+++ cypress/e2e/teste-automatico/teste-faturamento.cy.js
@@ -62,6 +62,55 @@ context('', () => {
             .type('1000', {force:true});
         cy.get('#dropdownMenuButton').click({force:true})
         cy.get('[ng-reflect-translate="HORAS"]').click({force:true})
+        cy.get(':nth-child(3) > .form-group > .row > .form-control').select(0)
+        cy.get('.col-8 > .form-control').trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .type('15000', {force:true});
+        cy.get('.form-group > .btn').click({force:true})
+        cy.get(':nth-child(2) > .table-responsive').should('include.text', 'Tipo de Horas')
+        cy.get('#button-basic').click({force:true}).wait(500)
+        cy.get('.search-drop-cat-multi').type('Todos', {force:true})
+        cy.get('.category-btn-not-child').click({force:true})
+        cy.get(':nth-child(1) > :nth-child(4) > .form-control').trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .type('1000', {force:true});
+        cy.get(':nth-child(2) > :nth-child(4) > .form-control').trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .type('2000', {force:true});
+        cy.get('app-modal-cobranca-hora > .modal-content > .modal-footer > .btn').click({force:true})
+        cy.get(':nth-child(5) > .col-12').should('not.be.empty')
+        cy.get('.text-center > .btn').click({force:true})
+        cy.get('.list > .ng-star-inserted > [ng-reflect-translate="HORAS"]').should('have.text', 'Horas')
+        cy.get('.col-12 > .table-responsive > .table > .list > .ng-star-inserted > :nth-child(2)').should('not.be.empty')
+        cy.get('#dropdownMenuButton').click({force:true})
+        cy.get('[ng-reflect-translate="OUTROS"]').click({force:true})
+        cy.get('app-modal-cobranca-outros-geral > .modal-content > .modal-body').should('not.be.empty')
+        cy.get('.col-2 > .btn').click({force:true})
+        cy.get('app-modal-cobranca-outros > .modal-content > .modal-header').should('have.text', 'Novo serviço')
+        cy.get('.modal-body > :nth-child(1) > .col-12 > .form-group > select-padrao > .custom > .ng-select-container > .ng-value-container > .ng-input > input').click({force:true})
+            .select(0)
+        cy.xpath('/html/body/ngb-modal-window[3]/div/div/app-modal-cobranca-outros/div/div[2]/div[2]/div[1]/div/div/datepicker-padrao/div/input')
+            .click({force:true}).type(`${dataHoje}{enter}`, {force:true})
+        cy.get(':nth-child(2) > :nth-child(2) > .form-group > .form-control').trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .type('5000', {force:true});
+        cy.get('.modal-body > :nth-child(1) > .col-12 > .form-group > select-padrao > .custom > .ng-select-container > .ng-value-container > .ng-input > input').click({force:true})
+            .select(1)
+        cy.xpath('/html/body/ngb-modal-window[3]/div/div/app-modal-cobranca-outros/div/div[2]/div[2]/div[1]/div/div/datepicker-padrao/div/input')
+            .click({force:true}).type(`${dataHoje}{enter}`, {force:true})
+        cy.get(':nth-child(2) > :nth-child(2) > .form-group > .form-control').trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .trigger('mousedown', { button: 0 }).trigger('mouseup', { button: 0 })
+            .type('5000', {force:true});
+        cy.get('.mt-3 > .col-12').should('have.length', 2)
+        cy.get('.text-center > .btn').click({force:true})
+        cy.get('.col-12 > .table-responsive > .table').should('have.text', 'Outros')
+
+
 
 
         
