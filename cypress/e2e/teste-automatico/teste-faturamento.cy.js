15fd52f374e2bd6e6d144e8b19f8dadb6c742896
Lucas <lucas.carvalho@milvus.com.br>
2023-11-22 12:34:24 -0300

    Adicionada cobrança de dispositivos/ Cobrança avulsa

--- cypress/e2e/teste-automatico/teste-faturamento.cy.js
+++ cypress/e2e/teste-automatico/teste-faturamento.cy.js
@@ -64,6 +64,8 @@ context('', () => {
             .trigger('mousedown', { button: 0, force: true })
             .trigger('mouseup', { button: 0, force: true })
             .type('1000', {force:true});
+        
+            //Cobrança Horas
         cy.get('#dropdownMenuButton').click({force:true})
         cy.get('[ng-reflect-translate="HORAS"]').click({force:true})
         cy.wait(1000)
@@ -96,6 +98,28 @@ context('', () => {
         cy.get('.text-center > .btn').click({force:true})
         cy.get('.list > .ng-star-inserted > [ng-reflect-translate="HORAS"]').should('have.text', 'Horas')
         cy.get('.col-12 > .table-responsive > .table > .list > .ng-star-inserted > :nth-child(2)').should('not.be.empty')
+        
+        //Cobrança Dispositivos
+        cy.get('#dropdownMenuButton').click({force:true})
+        cy.get('[ng-reflect-translate="DISPOSITIVOS"]').click({force:true})
+        cy.get(':nth-child(1) > .form-group > .form-control').select(0, {force:true})
+        cy.get('.mt-2 > :nth-child(2) > .form-group > .form-control').trigger('mouseup', { button: 0, force: true })
+            .trigger('mousedown', { button: 0, force: true })
+            .trigger('mouseup', { button: 0, force: true })
+            .trigger('mousedown', { button: 0, force: true })
+            .trigger('mouseup', { button: 0, force: true })
+            .type('3000', {force:true});
+        cy.get(':nth-child(1) > :nth-child(2) > .form-control').select(0, {force:true})
+        cy.get(':nth-child(1) > :nth-child(3) > .form-control').click({force:true}).type(0, {force:true})
+        cy.get(':nth-child(1) > :nth-child(4) > .form-control').trigger('mouseup', { button: 0, force: true })
+            .trigger('mousedown', { button: 0, force: true })
+            .trigger('mouseup', { button: 0, force: true })
+            .trigger('mousedown', { button: 0, force: true })
+            .trigger('mouseup', { button: 0, force: true })
+            .type('5000', {force:true});
+        cy.get('app-modal-cobranca-dispositivos > .modal-content > .modal-footer > .btn').click({force:true})
+        
+        //Cobrança Outros
         cy.get('#dropdownMenuButton').click({force:true})
         cy.get('[ng-reflect-translate="OUTROS"]').click({force:true})
         cy.get('app-modal-cobranca-outros-geral > .modal-content > .modal-body').should('not.be.empty')
@@ -127,19 +151,40 @@ context('', () => {
         cy.get('div.row:nth-child(3) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody').find('tr')
             .should('have.length', 3)
         cy.get('.text-center > .btn').click({force:true})
-        cy.get('tbody.ng-star-inserted').find('tr').should('have.length', 3)
+        cy.get('tbody.ng-star-inserted').find('tr').should('have.length', 4)
         cy.get('.modal-footer > .btn').click({force:true})
         cy.wait(1000)
-        /*cy.get(':nth-child(1) > :nth-child(1) > .text-filters > .nome-filtro').click({force:true})
-        cy.get('#searchDropdown0').click({force:true})
-            .type('Milvus inovacoes em software')
-            .wait(500)
-        cy.get('#opt-0-105798 > .nome-filtro').click({force:true})
-        cy.get('.filtro-controls > :nth-child(2) > .btn').click({force:true})*/
         cy.get(':nth-child(1) > .text-right > .btn-danger').click({force:true})
         cy.get('#swal2-title').should('be.visible').should('have.text', 'Deseja excluir este item?')
         cy.get('.swal2-confirm').click({force:true})
+        cy.wait(1000)
 
+        //Cobrança Avulsa
+        cy.visit('https://mastermilvusui.milvus.com.br/revenues/charges')
+        cy.get('.breadcrumb').should('include.text', 'Gerenciamento de Cobranças')
+        cy.get('.col-5 > .btn-success').click({force:true}).wait(2000)
+        cy.get(':nth-child(1) > .form-group > select-padrao > .custom > .ng-select-container > .ng-value-container > .ng-input > input')
+            .click({force:true})
+            .wait(2000)
+            .type('Milvus inovacoes em software', {force:true}).wait(3000).type('{enter}', {force:true}).wait(3000)
+        cy.get('.form-group.ng-star-inserted > select-padrao > .custom > .ng-select-container > .ng-value-container > .ng-input > input')
+            .click({force:true})
+            .type('Milvus{enter}', {force:true}).click({force:true}).wait(3000)
+        cy.xpath('/html/body/ngb-modal-window/div/div/app-cobrancas-modal-nova-cobranca/div/div[2]/form/div[2]/div[1]/datepicker-padrao/div/input').click({force:true})
+            .type(`${dataHoje}{enter}`, {force:true})
+        cy.get(':nth-child(2) > :nth-child(2) > select-padrao > .custom > .ng-select-container > .ng-value-container > .ng-input > input')
+            .click({force:true})
+            .type('Real{enter}', {force:true}).wait(500)
+        cy.get(':nth-child(3) > .form-control').click({force:true})
+            .type('5000', {force:true}).wait(500)
+        cy.get(':nth-child(2) > select-padrao.ng-star-inserted > .custom > .ng-select-container > .ng-value-container > .ng-input > input')
+            .click({force:true})
+            .type('Nota Fiscal e Boleto{enter}', {force:true}).wait(500)
+        cy.get(':nth-child(3) > select-padrao.ng-star-inserted > .custom > .ng-select-container > .ng-value-container > .ng-input > input').click({force:true})
+            .wait(500)
+        cy.get('.ng-dropdown-panel-items').first().click({force:true});
+        cy.get('.modal-footer > .btn').click({force:true})
+        
 
 
 
